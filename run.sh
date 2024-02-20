674c32188615259aa6653538d7c8c31c5f8364aa
Johnny Bravo <hakmonkey@proton.me>
2024-02-20 03:58:34 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,7 +29,4 @@ module load snakemake
 ##----------------------------------------------------------
 # Run
 
-snakemake --cluster "sbatch -A {cluster.account} -q {cluster.qos} -c {cluster.cpus-per-task} -N {cluster.Nodes} \
-  -t {cluster.runtime} --mem {cluster.mem} -J {cluster.jobname} --mail-type={cluster.mail_type} \
-  --mail-user={cluster.mail} --output {cluster.out} --error {cluster.err}" \
-  --cluster-config config/cluster.json --jobs 100 --latency-wait 20 --rerun-incomplete --use-conda
+snakemake --profiles profiles/slurm
