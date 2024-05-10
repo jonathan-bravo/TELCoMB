4e1bfed7377568a213008e5216a719e0a3e9f574
Johnny Bravo <hakmonkey@proton.me>
2024-05-10 14:00:56 -0400

    Update colocalizations_notebook.py

--- workflow/scripts/colocalizations_notebook.py
+++ workflow/scripts/colocalizations_notebook.py
@@ -16,44 +16,6 @@ def parse_args():
     parser.add_argument('--colocalizations')
     parser.add_argument('--output_plot')
     return parser.parse_args()
-
-
-# def read_megares_v2_ontology(config):
-#     # Create ontology dictionary from MEGARes ontology file
-#     megares_ontology = dict()
-#     hierarchy_dict = dict()
-#     with open(config['DATABASE']['MEGARES_ONTOLOGY'], 'r') as ontology_tsv:
-#         ontology_reader = csv.reader(ontology_tsv)
-#         for row in ontology_reader:
-#             # Skip column names
-#             if row[0] == "header":
-#                 continue
-
-#             typ = row[1]
-#             cl = row[2]
-#             mech = row[3]
-#             group = row[4]
-
-#             # Set up hiearachy dict. This will be our tree structure
-#             if not typ in hierarchy_dict:
-#                 hierarchy_dict[typ] = {}
-
-#             if not cl in hierarchy_dict[typ]:
-#                 hierarchy_dict[typ][cl] = {}
-
-#             if not mech in hierarchy_dict[typ][cl]:
-#                 hierarchy_dict[typ][cl][mech] = []
-
-#             if not group in hierarchy_dict[typ][cl][mech]:
-#                 hierarchy_dict[typ][cl][mech].append(group)
-
-#             # FIll in our dict
-#             megares_ontology[row[0]] = {"class": cl,
-#                                         "mechanism": mech,
-#                                         "group": group,
-#                                         "type": typ
-#                                         }
-#     return megares_ontology, hierarchy_dict
     
 
 def add_genes_to_plot(gene_list, color, label, axis):
@@ -64,13 +26,15 @@ def add_genes_to_plot(gene_list, color, label, axis):
             gene_label = gene.split('|')[4]
         elif label == 'MGE':
             gene_label = gene
-        element_r = Rectangle((start, curr_y), length, height, facecolor=color, alpha=0.8, edgecolor='black',label=label)
+        element_r = Rectangle((start, curr_y), length, height, facecolor=color,
+                              alpha=0.8, edgecolor='black',label=label)
         axis.add_patch(element_r)
 
         rx, ry = element_r.get_xy()
         cx = rx + element_r.get_width() / 2.0
         cy = ry + element_r.get_height() / 2.0
-        axis.annotate(gene_label, (cx, cy - (height / 2 + 1)), color='black', fontsize=8, ha='center',va='center')
+        axis.annotate(gene_label, (cx, cy - (height / 2 + 1)), color='black',
+                      fontsize=8, ha='center',va='center')
 
 
 def main():
@@ -94,9 +58,6 @@ def main():
 
     ARG_COLOR  = 'red'
     MGE_COLOR  = 'green'
-    # READ_COLOR = 'blue'
-
-    # megares_ontology, _ = read_megares_v2_ontology(config)
 
     colocalizations_list = [row for row in csv.reader(open(colocalizations, 'rt'))][1:]
 
@@ -110,8 +71,8 @@ def main():
         read_name = row[0]
         read_length = reads_lengths[read_name]
         used_reads_lengths.append(read_length)
-        # read_r = Rectangle((0, curr_y), read_length, height, facecolor='blue', alpha=0.8, edgecolor='black', label='Read')
-        read_r = Rectangle((0, curr_y+0.75), read_length, 0.5, facecolor='black', alpha=0.8, edgecolor='black', label='Read')
+        read_r = Rectangle((0, curr_y+0.75), read_length, 0.5, facecolor='black',
+                           alpha=0.8, edgecolor='black', label='Read')
         currentAxis.add_patch(read_r)
 
         # Get ARGs, MGEs, KEGGs
@@ -128,7 +89,8 @@ def main():
 
 
     sns.despine(top=True, right=True, left=True, bottom=False)
-    plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+    plt.tick_params(axis='y', which='both', left=False, right=False,
+                    labelleft=False)
     plt.xlim([0, max(used_reads_lengths)])
     plt.ylim([0, curr_y])
     # plt.grid(axis='x', color='0.95')
