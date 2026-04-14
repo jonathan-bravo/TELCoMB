e279e5c391f58c34b69fe1535e6cb78c6a3cb30d
Peter Ferm <fermx014@umn.edu>
2026-04-14 08:35:06 -0500

    Add flowcell id to the telcomb stats file Add read counts to the intermediate fastq files

--- workflow/rules/telcomb_stats.smk
+++ workflow/rules/telcomb_stats.smk
@@ -4,7 +4,13 @@ rule amr_mges_on_target:
        amr_file=f"{OUTDIR}/{{sample}}_deduplicated_amr_features.csv",
        mges_file=f"{OUTDIR}/{{sample}}_deduplicated_mobilome.csv",
        raw_read_file=f"{OUTDIR}/{{sample}}_start_read_count.txt",
+       trim_file=f"{OUTDIR}/{{sample}}_pre_dedup_rl.tsv",
+       hard_trim_file=f"{OUTDIR}/{{sample}}_hard_trim_count.txt",
+       chimeric_file=f"{OUTDIR}/{{sample}}_chimeric_count.txt",
+       dedup_file=f"{OUTDIR}/{{sample}}_post_dedup_rl.tsv",
+       duplicate_file=f"{OUTDIR}/{{sample}}_duplicates.txt",
        non_host_file=f"{OUTDIR}/{{sample}}_non_host_read_count.txt",
+       flowcell=FLOWCELL,
     output:
         f"{OUTDIR}/{{sample}}_telcomb_stats.csv",
     conda:
@@ -19,7 +25,13 @@ rule amr_mges_on_target:
         "{input.amr_file} "
         "{input.mges_file} "
         "{input.raw_read_file} "
+        "{input.trim_file} "
+        "{input.hard_trim_file} "
+        "{input.chimeric_file} "
+        "{input.dedup_file} "
+        "{input.duplicate_file} "
         "{input.non_host_file} "
+        "{input.flowcell} "
         "{output}"
 
 rule cat_telcomb_stats:
@@ -48,5 +60,5 @@ rule add_header_telcomb_stats:
     log:
         f"{LOGDIR}/header_telcomb_stats.log",
     shell:
-        "sed '1i barcode,ARGs,MGEs,raw_reads,non_host_reads,raw_read_on_target_amr,raw_read_on_target_mges,non_host_read_on_target_amr,non_host_read_on_target_mges' {input} > {output}"
+        "sed '1i flowcell_id,barcode,ARGs,MGEs,raw_read_count,trim_read_count,hard_trim_read_count,chimeric_read_count,dedup_read_count,duplicate_read_count,non_host_read_rount,raw_read_on_target_amr,raw_read_on_target_mges,non_host_read_on_target_amr,non_host_read_on_target_mges' {input} > {output}"
 
--- workflow/scripts/telcomb_stats.sh
+++ workflow/scripts/telcomb_stats.sh
@@ -4,35 +4,74 @@ sample_filename=$1
 amr_filename=$2
 mges_filename=$3
 raw_read_filename=$4
-non_host_read_filename=$5
-out=$6
+trim_filename=$5
+hard_trim_filename=$6
+chimeric_filename=$7
+dedup_filename=$8
+duplicate_filename=$9
+non_host_read_filename=${10}
+flowcell_filename=${11}
+out=${12}
 
+# Sample name
 sample_name="${sample_filename##*/}"
 sample_name="${sample_name%%_*}"
 echo "$sample_name"
 
+# AMR count
 amr_count=$(grep -w "ARG_NUM_OF_READS" "$amr_filename" | cut -d, -f2 | tr -d '\r')
 echo "$amr_count"
 
+# MGE count
 mges_count=$(grep "MGES_NUM_OF_READS" "$mges_filename" | cut -d, -f2 | tr -d '\r')
 echo "$mges_count"
 
+# Raw read count
 raw_read_count=$(cat $raw_read_filename)
 echo "$raw_read_count"
 
-non_host_read_count=$(cat $non_host_read_filename)
+# Trim read count
+trim_read_count=$(echo $(cat "$trim_filename" | wc -l) - 1 | bc)
+echo "$trim_read_count"
+
+# Hard trim read count
+hard_trim_read_count=$(cat "$hard_trim_filename")
+echo "$hard_trim_read_count"
+
+# Chimeric read count
+chimeric_read_count=$(cat "$chimeric_filename")
+echo "$chimeric_read_count"
+
+# Dedup read count
+dedup_read_count=$(echo $(cat "$dedup_filename" | wc -l) - 1 | bc)
+echo "$dedup_read_count"
+
+# Duplicate read count
+duplicate_read_count=$(echo $(cat "$duplicate_filename" | wc -l) | bc)
+echo "$duplicate_read_count"
+
+# Non-host read count
+non_host_read_count=$(cat "$non_host_read_filename")
 echo "$non_host_read_count"
 
+# AMR on target raw reads
 amr_ontarget_raw_read=$(echo "$amr_count $raw_read_count" | awk '{print $1 / $2}')
 echo $amr_ontarget_rawread
 
+# MGEs on target raw reads
 mges_ontarget_raw_read=$(echo "$mges_count $raw_read_count" | awk '{print $1 / $2}')
 echo $mges_ontarget_raw_read
 
+# AMR on target non-host reads
 amr_ontarget_non_host_read=$(echo "$amr_count $non_host_read_count" | awk '{print $1 / $2}')
 echo $amr_ontarget_non_host_read
 
+# MGEs on target non-host reads
 mges_ontarget_non_host_read=$(echo "$mges_count $non_host_read_count" | awk '{print $1 / $2}')
 echo $mges_ontarget_non_host_read
 
-printf '%s,%s,%s,%s,%s,%f,%f,%f,%f\n' "$sample_name" "$amr_count" "$mges_count" "$raw_read_count" "$non_host_read_count" "$amr_ontarget_raw_read" "$mges_ontarget_raw_read" "$amr_ontarget_non_host_read" "$mges_ontarget_non_host_read" > "$out"
+flowcell_id=$(grep "flow_cell_id=" "$flowcell_filename" | cut -d= -f2-)
+echo $flowcell_id
+
+# Concat counts
+printf '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%f,%f,%f,%f\n' "$flowcell_id" "$sample_name" "$amr_count" "$mges_count" "$raw_read_count" "$trim_read_count" "$hard_trim_read_count" "$chimeric_read_count" "$dedup_read_count" "$duplicate_read_count" "$non_host_read_count" "$amr_ontarget_raw_read" "$mges_ontarget_raw_read" "$amr_ontarget_non_host_read" "$mges_ontarget_non_host_read" > "$out"
